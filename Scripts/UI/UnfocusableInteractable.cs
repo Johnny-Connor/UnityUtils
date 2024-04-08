8038cf24563aba095249e65184506b04cc6abda1
Johnny-Connor <42523848+Johnny-Connor@users.noreply.github.com>
2024-04-08 17:21:56 -0300

    Update UnfocusableInteractable.cs
    - Improved description.
    - Added customizable ColorBlock variable.
    - Added validation against Selectables components.
    - Added normal/disabled color handling in Awake/OnValidate.
    - Added event for each interaction phase.

--- Scripts/UI/UnfocusableInteractable.cs
+++ Scripts/UI/UnfocusableInteractable.cs
@@ -2,9 +2,13 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
- 
+
 [RequireComponent(typeof(Image))]
-// An interactable that does not take the focus from a selected Selectable upon interaction.
+/*
+An interactable that does not take the focus from a selected Selectable upon interaction.
+Note: If the GameObject containing this script has any other script with the ISelectHandler interface in it, 
+the EventSystem will clear the selection whenever this interactable is clicked.
+*/
 public class UnfocusableInteractable :
     MonoBehaviour,
     IPointerClickHandler,
@@ -14,56 +18,88 @@ public class UnfocusableInteractable :
     IPointerUpHandler
 {
     // Variables.
+    [SerializeField] private bool _interactable = true;
+    [SerializeField] private ColorBlock _colorBlock = ColorBlock.defaultColorBlock;
     private Image _interactableImage;
-    private bool _interactable = true;
- 
- 
+
+
     // Properties.
     public bool Interactable
     {
         get => _interactable;
         set
         {
-            _interactableImage.color = value ?
-                ColorBlock.defaultColorBlock.normalColor :
-                ColorBlock.defaultColorBlock.disabledColor
-            ;
             _interactable = value;
+            _interactableImage.color = _interactable ? _colorBlock.normalColor : _colorBlock.disabledColor;
         }
     }
- 
- 
+
+
     // Events.
-    public Action OnClick;
- 
- 
-    // MonoBehaviour.
-    private void Awake() => _interactableImage = GetComponent<Image>();
- 
- 
+    public event EventHandler OnPointerClicked;
+    public event EventHandler OnPointerDowned;
+    public event EventHandler OnPointerEntered;
+    public event EventHandler OnPointerExited;
+    public event EventHandler OnPointerUped;
+
+
+    // MonoBehaviour Methods.
+    private void Awake()
+    {
+        if (GetComponent<Selectable>())
+            Debug.LogError($"Do not attach {nameof(Selectable)}s to {nameof(UnfocusableInteractable)}s.")
+        ;
+
+        if (!_interactableImage) _interactableImage = GetComponent<Image>();
+
+        Color firstColor = _interactable ? _colorBlock.normalColor : _colorBlock.disabledColor;
+        _interactableImage.color = firstColor;
+    }
+
+    #if UNITY_EDITOR 
+    private void OnValidate() => Awake();
+    #endif
+
+
     // Interface Methods.
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (Interactable) OnClick?.Invoke();
+        if (Interactable) OnPointerClicked?.Invoke(this, EventArgs.Empty);
     }
- 
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (Interactable) _interactableImage.color = ColorBlock.defaultColorBlock.pressedColor;
+        if (!Interactable) return;
+
+        _interactableImage.color = _colorBlock.pressedColor;
+
+        OnPointerDowned?.Invoke(this, EventArgs.Empty);
     }
- 
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (Interactable) _interactableImage.color = ColorBlock.defaultColorBlock.highlightedColor;
+        if (!Interactable) return;
+
+        _interactableImage.color = _colorBlock.highlightedColor;
+
+        OnPointerEntered?.Invoke(this, EventArgs.Empty);
     }
- 
+
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (Interactable) _interactableImage.color = ColorBlock.defaultColorBlock.normalColor;
+        if (!Interactable) return;
+
+        _interactableImage.color = _colorBlock.normalColor;
+
+        OnPointerExited?.Invoke(this, EventArgs.Empty);
     }
- 
+
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (Interactable) _interactableImage.color = ColorBlock.defaultColorBlock.selectedColor;
+        if (!Interactable) return;
+
+        _interactableImage.color = _colorBlock.selectedColor;
+
+        OnPointerUped?.Invoke(this, EventArgs.Empty);
     }
 }
