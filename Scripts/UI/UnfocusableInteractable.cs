bae60bd5d6afd4dbfa35b034d2bdc23d937231f1
Johnny-Connor <42523848+Johnny-Connor@users.noreply.github.com>
2024-09-19 18:16:55 -0300

    Update UnfocusableInteractable.cs
    - Added support for the 'Color Multiplier' and 'Fade Duration' properties of the ColorBlock.

--- Scripts/UI/UnfocusableInteractable.cs
+++ Scripts/UI/UnfocusableInteractable.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 /*
 An interactable that does not take the focus from a selected Selectable upon interaction.
-Note: If the GameObject containing this script has any other script with the ISelectHandler interface in it, 
-the EventSystem will clear the selection whenever this interactable is clicked.
+Note: If the GameObject containing this script has any other script with the ISelectHandler interface in 
+it, the EventSystem will clear the selection whenever this interactable is clicked.
 */
+[ExecuteAlways]
 public class UnfocusableInteractable:
-    MonoBehaviour,
+    UIBehaviour,
     IPointerClickHandler,
     IPointerDownHandler,
     IPointerEnterHandler,
@@ -53,24 +54,27 @@ public class UnfocusableInteractable:
 
 
     // MonoBehaviour Methods.
-    private void Awake()
-    {
-        if (GetComponent<Selectable>())
-            Debug.LogError($"Do not attach {nameof(Selectable)}s to {nameof(UnfocusableInteractable)}s.")
-        ;
-
-        UpdateGraphicColor();
-    }
-
-    private void OnValidate() => UpdateGraphicColor();
+    protected override void Awake() => UpdateGraphicColor();
+    protected override void OnValidate() => UpdateGraphicColor();
+    protected override void OnEnable() => StartColorTween(_colorBlock.normalColor, true);
+    protected override void OnDisable() => StartColorTween(Color.white, true);
 
 
     // Methods.
-    private void UpdateGraphicColor()
+    private void UpdateGraphicColor() => 
+        StartColorTween(_interactable ? _colorBlock.normalColor : _colorBlock.disabledColor, true)
+    ;
+
+    private void StartColorTween(Color targetColor, bool instant = false)
     {
-        if (_targetGraphic)
-            _targetGraphic.color = _interactable ? _colorBlock.normalColor : _colorBlock.disabledColor
-        ;
+        if (!_targetGraphic) return;
+
+        _targetGraphic.CrossFadeColor(
+            targetColor * _colorBlock.colorMultiplier, 
+            instant ? 0f : _colorBlock.fadeDuration, 
+            true, 
+            true
+        );
     }
 
 
@@ -83,36 +87,28 @@ public class UnfocusableInteractable:
     public void OnPointerDown(PointerEventData eventData)
     {
         if (!Interactable) return;
-
-        if (_targetGraphic) _targetGraphic.color = _colorBlock.pressedColor;
-
+        StartColorTween(_colorBlock.pressedColor);
         OnPointerDowned?.Invoke(this, EventArgs.Empty);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (!Interactable) return;
-
-        if (_targetGraphic) _targetGraphic.color = _colorBlock.highlightedColor;
-
+        StartColorTween(_colorBlock.highlightedColor);
         OnPointerEntered?.Invoke(this, EventArgs.Empty);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         if (!Interactable) return;
-
-        if (_targetGraphic) _targetGraphic.color = _colorBlock.normalColor;
-
+        StartColorTween(_colorBlock.normalColor);
         OnPointerExited?.Invoke(this, EventArgs.Empty);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         if (!Interactable) return;
-
-        if (_targetGraphic) _targetGraphic.color = _colorBlock.selectedColor;
-
+        StartColorTween(_colorBlock.selectedColor);
         OnPointerUped?.Invoke(this, EventArgs.Empty);
     }
 }
